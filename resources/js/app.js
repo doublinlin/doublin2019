6e5cb7c99e4efa9fca2fe9ef8a658bef94b4c6e6
Doublin <doublin@DoublindeMacBook-Pro.local>
2020-01-18 17:41:04 +0800

    20200118 - import BootstrapVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,16 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 require('./bootstrap');
 
@@ -19,9 +29,9 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('language-switcher', require('./components/LanguageSwitcher.vue').default);
 Vue.component('home', require('./components/Home.vue').default);
+Vue.component('itsme', require('./components/ItsMe.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/ItsMe.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-body">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
