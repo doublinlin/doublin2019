6216e415351310cb07683b57bf52d8f5848197f5
Doublin <doublinlin@gmail.comgit config --global user.name pin>
2020-08-28 23:47:00 +0800

    20200828 - debug cross-env, flag svg

--- resources/js/components/LanguageSwitcher.vue
+++ resources/js/components/LanguageSwitcher.vue
@@ -10,7 +10,7 @@
       @keydown.up.exact.prevent="startArrowKeys"
       @keydown.down.exact.prevent="startArrowKeys"
     >
-      <img :src="`/images/flag_${locale}.svg`" alt="flag" class="w-8 h-8">
+      <img :src="`http://doublinlin.com/doublin/doublin/public/images/flag_${locale}.png`" alt="flag" class="w-8 h-8">
       <span class="ml-2">{{ locale.toUpperCase() }}</span>
       <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"></path></svg>
     </button>
@@ -26,7 +26,7 @@
             @keydown.down.exact.prevent="focusNext(true)"
             @keydown.esc.exact="hideDropdown"
           >
-            <img src="/images/flag_en.svg" alt="english flag" class="h-8 w-8">
+            <img src="http://doublinlin.com/doublin/doublin/public/images/flag_en.png" alt="english flag" class="h-8 w-8">
             <span class="ml-2">English</span>
           </a>
         </li>
@@ -54,7 +54,7 @@
             @keydown.tab.exact="hideDropdown"
             @keydown.esc.exact="hideDropdown"
           >
-            <img src="/images/flag_zh.svg" alt="english flag" class="h-8 w-8">
+            <img src="http://doublinlin.com/doublin/doublin/public/images/flag_zh.png" alt="english flag" class="h-8 w-8">
             <span class="ml-2">Chinese</span>
           </a>
         </li>
